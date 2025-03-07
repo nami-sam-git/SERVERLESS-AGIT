ade6a22e15bfe9bd84622c5cd0855759afac9d63
dihkaw <162821190+dihkaw@users.noreply.github.com>
2025-03-07 15:30:45 +0700

    Update sqs.py

--- lambda/sqs.py
+++ lambda/sqs.py
@@ -1,49 +1,34 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
+
+# Inisialisasi klien DynamoDB
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('BukuTamuTable')
 
 def lambda_handler(event, context):
-    # Membuat klien SQS
-    sqs = boto3.client('sqs')
-    queue_url = 'https://sqs.xxxxxxxxxxxxxxxxxxxxxxx/BukuTamuQueue'  # Ganti dengan URL SQS Anda
-
     try:
-        # Parse data dari body request
-        body = json.loads(event['body'])
-        nama = body['nama']
-        pesan = body['pesan']
-
-        # Generate ID unik (contoh menggunakan timestamp)
-        import time
-        id = str(int(time.time()))
-
-        # Buat pesan untuk dikirim ke SQS
-        message = {
-            'id': id,
-            'nama': nama,
-            'pesan': pesan
-        }
-
-        # Kirim pesan ke SQS
-        sqs.send_message(
-            QueueUrl=queue_url,
-            MessageBody=json.dumps(message)
-        )
-
-        # Mengembalikan respons sukses
+        # Loop melalui setiap pesan dalam event (SQS bisa mengirim batch pesan)
+        for record in event['Records']:
+            # Parse pesan dari SQS
+            message = json.loads(record['body'])
+            id = message['id']
+            nama = message['nama']
+            pesan = message['pesan']
+            
+            # Simpan data ke DynamoDB
+            table.put_item(Item={
+                'id': id,
+                'nama': nama,
+                'pesan': pesan
+            })
+        
         return {
             'statusCode': 200,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'  # Izinkan akses dari semua domain
-            },
-            'body': json.dumps({'message': 'Data tamu berhasil dikirim ke antrian!', 'id': id})
+            'body': json.dumps({'message': 'Data tamu berhasil diproses!'})
         }
-    except Exception as e:
+    except ClientError as e:
         return {
             'statusCode': 500,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
             'body': json.dumps({'message': str(e)})
         }
